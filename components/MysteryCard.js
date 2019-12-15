3b5157edbbdb4c0576f371c540b6248c25536b27
dani <mayor@dani.town>
2019-12-14 19:53:39 -0800

    added video controls but they dont work

--- components/MysteryCard.js
+++ components/MysteryCard.js
@@ -5,7 +5,25 @@ const MysteryCard = props => (
     </div>
     <div className="card-content">
       <div>
-        <img src="https://starboard-media.s3.amazonaws.com/v/no-wm-thumb-y9RSZSnG9J-00001.jpg" />
+        <video>
+          <source src="https://d3el26csp1xekx.cloudfront.net/v/no-wm-y9RSZSnG9J.mp4" type="video/mp4" />
+        </video>
+        <div className="video-controls">
+          <div className="video-controls--div">
+          <span className="play-button">
+            <img src="https://d3el26csp1xekx.cloudfront.net/static/assets/video-cards-2.0/play.svg" alt="play icon" class="_3i_XRXKYAQRX0dKnCJx-Qt" />
+          </span>
+          <span className="video-length">1:21</span>
+          </div>
+        </div>
+        <div className="video-reactions">
+          <span className="react-button">
+            <img src="https://d3el26csp1xekx.cloudfront.net/static/assets/video-cards-2.0/message-circle-white.svg" alt="comment icon"/>
+          </span>
+          <span className="like-button">
+            <img src="https://d3el26csp1xekx.cloudfront.net/static/assets/video-cards-2.0/heart-white.svg" alt="like icon" class="_10uGAZviFOXWRnPf7dN4gH _2IOBfkgAx56DsQoZe9MsC9" />
+          </span>
+        </div>
       </div>
     </div>
   </div>
