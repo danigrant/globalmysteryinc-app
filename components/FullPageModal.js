a5a7d1c9a40f21231a148840cbe628e8122295dd
dani <mayor@dani.town>
2019-12-14 20:04:33 -0800

    a but brain dead

---
+++ components/FullPageModal.js
@@ -0,0 +1,7 @@
+const FullPageModal = props => (
+  <div className="full-page-modal">
+    <i className="material-icons close-modal-button">close_rounded</i>
+  </div>
+)
+
+export default FullPageModal
--- pages/newest.js
+++ pages/newest.js
@@ -1,9 +1,12 @@
 import Header from '../components/Header'
 import Grid from '../components/Grid'
+import Modal from 'boron/ScaleModal'
+import FullPageModal from '../components/FullPageModal'
 
 const BrowseNewestMysteries = () => (
   <div>
     <Header />
+    <FullPageModal />
     <div className="container margin-top-med margin-bottom-med">
       <h1>Help Select The Top Mysteries Of The Day...</h1>
       <h2>Browse recently submitted mysteries and upvote your favorites. The winners of the day will go on to be the mysteries to answer tomorrow. </h2>
